259e77dad8ff6c60f92810c3dc9c56a49cd733db
Esmatullah <esmatullah.hashimi2016@gmail.com>
2024-05-19 15:41:09 +0430

    dynamic meta data created

--- src/app/blog/[blogId]/page.tsx
+++ src/app/blog/[blogId]/page.tsx
@@ -1,3 +1,25 @@
-export default function blogDetails({params,}:{params:{blogId:string};}){
+
+import { Metadata } from "next";
+
+
+type Props = {
+    params:{
+        blogId: string;
+    };
+};
+export const generateMetadata = async ({ params, }: Props): Promise<Metadata> => {
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Social ${params.blogId}`);
+        }, 100);
+        
+    });
+    return {
+        title: `blog ${params.blogId}`,
+    };
+    
+};
+
+export default function BlogDetails({params}: Props){
     return <h1>Blog Details of blog ID {params.blogId}</h1>;
 }
