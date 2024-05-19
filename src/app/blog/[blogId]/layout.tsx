40719cd273843fec8a448ffdd44ca7b822298b21
Esmatullah <esmatullah.hashimi2016@gmail.com>
2024-05-19 14:38:40 +0430

    Nested layout added

---
+++ src/app/blog/[blogId]/layout.tsx
@@ -0,0 +1,9 @@
+export default function BlogDetailsLayout({children,}:{children: React.ReactNode;}){
+    return (
+        <>
+            {children}
+            <h2>Features Blogs</h2>
+        </>
+    );
+
+}
