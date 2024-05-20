49eb1e64539fe5c24c0333568c2d716a4bcc658d
Esmatullah <esmatullah.hashimi2016@gmail.com>
2024-05-20 14:53:43 +0430

    component link added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import Link from "next/link";
+
 export default function Home(){
-    return <h1>Welcome to Homepage !</h1>
+    const blogId = 100
+    return <div>
+        <h1>Welcome to Homepage !</h1>
+        <ul>
+            <li><Link href="/profile">Profile</Link></li>
+            <li><Link href="/about">About</Link></li>
+            <li><Link href={`blog/${blogId}`}>Blog</Link></li>
+            <li><Link href="/docs">Docs</Link></li>
+            <li><Link href="/register">Registration</Link></li>
+            <li><Link href="/login">Sign In</Link></li>
+            <li><Link href="/forgot-password">Forgot Password</Link></li>
+        </ul>
+    </div>
 }
