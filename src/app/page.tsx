c3532b0def92f3f61e3202abbdb04107d13db509
Esmatullah <esmatullah.hashimi2016@gmail.com>
2024-05-20 14:59:33 +0430

    list style added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ export default function Home(){
     const blogId = 100
     return <div>
         <h1>Welcome to Homepage !</h1>
-        <ul>
-            <li><Link href="/profile">Profile</Link></li>
-            <li><Link href="/about">About</Link></li>
-            <li><Link href={`blog/${blogId}`}>Blog</Link></li>
-            <li><Link href="/docs">Docs</Link></li>
-            <li><Link href="/register">Registration</Link></li>
-            <li><Link href="/login">Sign In</Link></li>
-            <li><Link href="/forgot-password">Forgot Password</Link></li>
+        <ul style={{listStyleType:"none",}}>
+            <li style={{padding:"10px"}}><Link href="/profile">Profile</Link></li>
+            <li style={{padding:"10px"}}><Link href="/about">About</Link></li>
+            <li style={{padding:"10px"}}><Link href={`blog/${blogId}`}>Blog</Link></li>
+            <li style={{padding:"10px"}}><Link href="/docs">Docs</Link></li>
+            <li style={{padding:"10px"}}><Link href="/register">Registration</Link></li>
+            <li style={{padding:"10px"}}><Link href="/login">Sign In</Link></li>
+            <li style={{padding:"10px"}}><Link href="/forgot-password">Forgot Password</Link></li>
         </ul>
     </div>
 }
