9a24db392940465d6d354aedf6c9daa1102fc0de
Esmatullah <esmatullah.hashimi2016@gmail.com>
2024-05-21 11:58:04 +0430

    Error handling added

--- src/app/blog/[blogId]/reviews/[reviewId]/page.tsx
+++ src/app/blog/[blogId]/reviews/[reviewId]/page.tsx
@@ -1,5 +1,8 @@
 import { notFound } from "next/navigation";
 
+function getRandomInt(count: number) {
+    return Math.floor(Math.random() * count);
+}
 export default function BlogReviews(
     {params,}: {
         params:{
@@ -7,10 +10,25 @@ export default function BlogReviews(
             blogId: string;
         }
     }) {
-        if(parseInt(params.reviewId) > 1000){
-            throw new Error("Error Loading Review...");
+        const random = getRandomInt(3);
+        if(random === 2){
+            throw new Error("Error too much refresh...wait for a moment and Tray again...");
         }
+        if(parseInt(params.reviewId) > 1000){
+            notFound();
+        }
+
         return (
-        <h1> Review {params.reviewId} for blog {params.blogId}</h1>
+            <>
+            <h1> Review {params.reviewId} for blog {params.blogId}</h1>
+            <div className={"text-red-300"}>
+                <h1>For observing the error handling in Next.js Refresh the page multiple times.</h1>
+                <ol>
+                    <li>If you refresh the page multiple times it gives an error</li>
+                   
+                </ol>
+            </div>
+            </>
+        
         );
 }
