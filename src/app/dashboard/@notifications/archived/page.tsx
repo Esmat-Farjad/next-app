b74acce79dce2637e961d434797503618dc1940d
Esmatullah <esmatullah.hashimi2016@gmail.com>
2024-05-22 11:40:31 +0430

    sub-navigation added

--- src/app/dashboard/@notifications/archived/page.tsx
+++ src/app/dashboard/@notifications/archived/page.tsx
@@ -1,11 +1,11 @@
 import Card from "@/components/card";
 import Link from "next/link";
-export default function Notifications(){
+export default function ArchivedNotifications(){
     return (
         <Card>
-            Default Notifications
+            <p>Archived Notifications</p>
             <button className={"bg-teal-500 px-2 py-1 rounded text-white hover:bg-teal-600 m-3"}>
-                <Link href="./dashboard/archived">Archived Notification</Link>
+                <Link href="/dashboard">Default Notification</Link>
             </button>
         </Card>
     );
