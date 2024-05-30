46ab5bccdddbc90f37485f5a38c1387e4d7cf06f
Esmatullah <esmatullah.hashimi2016@gmail.com>
2024-05-30 16:22:40 +0430

    styling added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home(){
     const blogId = 100
    
     return <div>
-        <div className="heading-container p-10 bg-white h-96 bg-gradient-to-b from-indigo-800 to-pink-700">
+        <div className="heading-container p-10 bg-white h-96 bg-gradient-to-b from-indigo-900 to-pink-800">
             {/* <Image src='next.svg' alt="this is the picture" width={100} height={100}/> */}
            <p className="text-white text-6xl font-bold text-center mt-10 pt-10">Welcome to my Portfolio</p>
            <div className="text-center">this is the subtext comes from the bottom of the page</div>
