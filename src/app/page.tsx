174d5692f2514ba92636892c3499ee3ba15814ea
Esmatullah <esmatullah.hashimi2016@gmail.com>
2024-05-30 16:18:18 +0430

    animation added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,14 @@
-import Link from "next/link";
-
+import Image from "next/image";
 export default function Home(){
     const blogId = 100
+   
     return <div>
-        <div className={"container"}>
-            <div className="grid grid-cols-5">
-                <div className={"border border-slate-500 p-3 rounded shadow"}>
-                    
-                </div>
-                <div className={"col-span-4"}>
-                    <div className={"header-text text-center"}>
-                        <p className={"text-4xl"}>Welcome to my Portfolio</p>
-                        <small>this is a demo text for description details </small>
-                        <p className="social-media">
-                            <Link href="/" className={"mx-3 hover:text-cyan-600"}>Facebook</Link>
-                            <Link href="/" className={"mx-3 hover:text-cyan-600"}>Twitter</Link>
-                            <Link href="/" className={"mx-3 hover:text-cyan-600"}>Instagram</Link>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        {/* <h1>Welcome to Homepage !</h1>
-
-        <ul style={{listStyleType:"none",}}>
-            <li style={{padding:"10px"}}><Link href="/profile">Profile</Link></li>
-            <li style={{padding:"10px"}}><Link href="/about">About</Link></li>
-            <li style={{padding:"10px"}}><Link href={`blog/${blogId}`}>Blog {blogId}</Link></li>
-            <li style={{padding:"10px"}}><Link href="/docs">Docs</Link></li>
-            <li style={{padding:"10px"}}><Link href="/register">Registration</Link></li>
-            <li style={{padding:"10px"}}><Link href="/login">Sign In</Link></li>
-            <li style={{padding:"10px"}}><Link href="/forgot-password" replace>Forgot Password</Link></li>
-        </ul> */}
+        <div className="heading-container p-10 bg-white h-96 bg-gradient-to-b from-indigo-800 to-pink-700">
+            {/* <Image src='next.svg' alt="this is the picture" width={100} height={100}/> */}
+           <p className="text-white text-6xl font-bold text-center">Welcome to my Portfolio</p>
+           <div className="text-center">this is the subtext comes from the bottom of the page</div>
         </div>
     </div>
+        
+
 }
