1e6da9d73ccf0151b57dcaad9b7cf0f3c585e9f8
Esmatullah <esmatullah.hashimi2016@gmail.com>
2024-05-30 16:26:36 +0430

    logo added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,9 @@ export default function Home(){
             {/* <Image src='next.svg' alt="this is the picture" width={100} height={100}/> */}
            <p className="text-white text-6xl font-bold text-center mt-10 pt-10">Welcome to my Portfolio</p>
            <div className="text-center">this is the subtext comes from the bottom of the page</div>
+           <div className="flex justify-center items-center">
+           <Image src="vercel.svg" className="p-5" alt="logo" width={500} height={500}></Image>
+           </div>
         </div>
     </div>
         
