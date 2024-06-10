1f2da9607b90f867a678342f55110a8069da9b58
Esmatullah <esmatullah.hashimi2016@gmail.com>
2024-06-10 15:37:13 +0430

    navigation added to sign up

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,6 +3,7 @@ import "./style.css";
 import Link from "next/link"; // used to navigate between component 
 import { usePathname } from "next/navigation"; // used to decalre the active path name 
 import { useState } from "react";
+import { GoChevronRight } from "react-icons/go";
 
 
 //navigation object for creating navigation menu of the following component
@@ -22,7 +23,11 @@ export default function AuthLayout({
     const [input, setInput] = useState("");
     return (
         <div className="mt-0">
-            <div>
+            <div className="mx-10">
+                <Link href="/" className="anchor-link mx-1">Home</Link>
+                <GoChevronRight className="inline text-gray-600 text-2xl"/>
+                <Link href="/register" className="anchor-link mx-1">Sign up</Link>
+
                 {/* this will be shared between compnent if you want separate the resource user template.tsx */}
                 {/* <input value={input} onChange={e => setInput(e.target.value)} /> */}
             </div>
