dc9b2a0fa14176b0d09ce13ffa42a4c2d618c9ce
Esmatullah <esmatullah.hashimi2016@gmail.com>
2024-06-11 15:59:01 +0430

    service page created

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,3 +1,26 @@
-export default function ServicePage({params:{id},}:{params: {id: string};}) {
-    return <h1 className="text-4xl">{id}</h1>;
+import serviceData, { ServiceType } from "../service";
+import Image from "next/image";
+import Link from "next/link";
+export default function ServicePage(
+    {
+        params:{id},
+    }:{
+        params: {id: string};
+    }) {
+        const photo: ServiceType = serviceData.find((p) => p.id === id) !;
+    return <div>
+        <div className="section-container">
+            <div className="grid grid-cols-2">
+                <div className="">
+                <Image src={photo.src} alt=''></Image>
+                </div>
+                <div className="">
+                    <p className="text-2xl font-bold">{photo.title}</p>
+                    <p className="">{photo.description}</p>
+                </div>
+            </div>
+            
+
+        </div>
+    </div>;
 }
