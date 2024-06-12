a68038bbc2d20c729bfeba224fc8a4c32bfb423f
Esmatullah <esmatullah.hashimi2016@gmail.com>
2024-06-12 10:53:37 +0430

    query form created

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { PiTrophy, PiUsers } from "react-icons/pi";
 import { LiaAwardSolid } from "react-icons/lia";
 import { IoBriefcaseOutline } from "react-icons/io5";
+import { FiUploadCloud } from "react-icons/fi";
 export default function ServicePage(
     {
         params:{id},
@@ -77,6 +78,49 @@ export default function ServicePage(
                                  bypassing the usual sales pitch.
                             </p>
                         </div>
+                    </div>
+                    <div className="bg-gray-500 p-3">
+                        <form action="#">
+                            <textarea name="proposal" id="proposal" className="form-input">
+                                Please describe your request - we usually respond within 1 hour on business days.
+                            </textarea>
+                            
+                            <div className="flex items-center justify-center w-full my-1">
+                                <label 
+                                htmlFor="dropzone-file" 
+                                className="flex flex-col items-center justify-center w-full h-24 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                                        <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                                        </svg>
+                                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                                        <p className="text-xs text-gray-500 dark:text-gray-400"> (MAX. 2.0 MB)</p>
+                                    </div>
+                                    <input id="dropzone-file" type="file" className="hidden" />
+                                </label>
+                            </div> 
+                            <p className="font-thin p-2">
+                                <FiUploadCloud className="inline mr-2 text-2xl"/>
+                                Drap and drop or browse to upload your file(s)
+                            </p>
+                            <div className="grid grid-cols-2 gap-5">
+                                <div className="grid-cols-1">
+                                    <input type="text" className="form-input" placeholder="Full Name"/>
+                                </div>
+                                <div className="grid-cols-1">
+                                <input type="text" className="form-input" placeholder="Company"/>
+                                </div>
+                                <div className="grid-cols-1">
+                                <input type="text" className="form-input" placeholder="Work email"/>
+                                </div>
+                                <div className="grid-cols-1">
+                                <input type="text" className="form-input" placeholder="Phone"/>
+                                </div>
+                            </div>
+                            <div className="text-center">
+                                <button className="btn-transparent mt-4">Submit</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
                 <div className="grid-cols-1">
